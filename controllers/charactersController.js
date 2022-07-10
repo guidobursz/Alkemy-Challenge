27c7d0aaa15ef243e576b2e8792bb5c686921581
gmb@ubuntu-main <76079195+guidobursz@users.noreply.github.com>
2022-07-09 21:01:18 -0300

    Description
    Done:
    Edit/Delete/~Show info of charactes by id

--- controllers/charactersController.js
+++ controllers/charactersController.js
@@ -1,7 +1,9 @@
 const Character = require('../db/models/Character');
 //For query
-const { Op } = require("sequelize");
+const { Op, where } = require("sequelize");
+const { findOne } = require('../db/models/Character');
 
+//Display characters. /api/character
 const characterIndex = async (req,res) => {
 /*
 Búsqueda de Personajes
@@ -35,8 +37,127 @@ Para especificar el término de búsqueda o filtros se deberán enviar como par�
 
     //FALTA:
         //query por pelis asociadas
-        // mostarar solo nombre/
 };
 
+//Display character info by ID. /api/character/:id/info
+const characterInfoGET = async (req,res) => {
+    /*    En el detalle deberán listarse todos los atributos del personaje, como así también sus películas o
+series relacionadas.
 
-module.exports = {characterIndex};
+FALTA: mostrar las peliculas en las que figura
+ */
+
+    if (req.params.id != undefined){
+        // req.params devuelve string, lo paso a number
+        let characterId = Number(req.params.id);
+    
+        //Get character info
+        let characterInfo = await Character.findOne({ where: { id: characterId } })
+        //Check if search is null
+            if (characterInfo === null) {
+                res.send('No character found')
+            } else {
+                res.json({characterInfo})
+            }
+    } else {
+        res.send('No character found')
+    }
+
+};
+
+//GET -> Create new character. /api/character/create
+const characterCreateGET = (req,res) => {
+    //Para crear nuevo character, necesito: ImageCharacter, name, age, weight, history
+    res.send('GET -> new Character form: need send body fields: ImageCharacter, name, age, weight, history')
+};
+//POST -> Create new character /api/character/create
+const characterCreatePOST = async (req,res) => {
+    //Para crear nuevo character, necesito: ImageCharacter, name, age, weight, history
+    let imageCharacter = req.body.image;
+    let name = req.body.name;
+    let age = req.body.age;
+    let weight = req.body.weight;
+    let history = req.body.history;
+
+    //Create new character & save in DB.
+    let newCharacter = Character.create({
+        imageCharacter,name,age,weight,history
+    }).then(res.json({'New Character': 'OK', imageCharacter,name,age,weight,history})
+    ).catch((e)=> res.send(e))
+
+};
+
+//PATCH => edit a character. /api/characters/:id/edit
+const characterEditPATCH = async (req,res) => {
+    // req.params devuelve string, lo paso a number
+    let characterId = Number(req.params.id);
+
+        //Recibo el o los campos a modificar en el body: name age weight history
+        let newName = req.body.name;
+        let newAge = req.body.age;
+        let newWeight = req.body.weight;
+        let newHistory = req.body.history;
+
+    //Check if all is undefined
+    if (newName == undefined && newAge == undefined && newWeight == undefined && newHistory == undefined) {
+        res.send(`Nothing to change in character id: ${req.params.id}`)
+    } else {
+                //Check undefined values, if is not undefined, will update
+                if (newName != undefined){
+                        let updateName = await Character.update({
+                            name: newName
+                        }, {where: {
+                                id: characterId
+                            }
+                        })
+                        //res.send('Complete update')
+                    }
+                if (newAge != undefined){
+                        let updateAge = await Character.update({
+                            age: newAge
+                        }, {where: {
+                                id: characterId
+                            }
+                        })
+                        //res.send('Complete update')
+                    }
+                if (newWeight != undefined){
+                        let updateWeight = await Character.update({
+                            weight: newWeight
+                        }, {where: {
+                                id: characterId
+                            }
+                        })
+                        //res.send('Complete update')
+                    }
+                if (newHistory != undefined){
+                        let updateHistory = await Character.update({
+                            history: newHistory
+                        }, {where: {
+                                id: characterId
+                            }
+                        })
+                        //res.send('Complete update')
+                    }
+        res.send('Character updated')
+    }
+}
+
+//DELETE => edit a character. /api/characters/:id/delete
+const characterDELETE = async (req, res) => {
+    // req.params devuelve string, lo paso a number
+    let characterId = Number(req.params.id);
+
+    //Delete query
+    let characterDelete = await Character.destroy({
+        where: {
+            id: characterId
+        }
+    })
+
+    res.send(`Character with id: ${req.params.id} was deleted`)
+
+}
+
+
+module.exports = {characterIndex, characterCreateGET, characterCreatePOST, characterEditPATCH, characterDELETE, characterInfoGET};
--- routes/characters.js
+++ routes/characters.js
@@ -2,8 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 //Import controllers
-const {characterIndex} = require('../controllers/charactersController');
+const {
+    characterIndex,
+    characterInfoGET,
+    characterCreateGET,
+    characterCreatePOST,
+    characterEditPATCH,
+    characterDELETE
+    } = require('../controllers/charactersController');
 
+
+//Display characters
 router.get('/', characterIndex)
+//Display info of :id character
+router.get('/:id/info', characterInfoGET)
+//Create new character
+    router.get('/create',characterCreateGET)
+    //POST
+    router.post('/create',characterCreatePOST)
+// Mod a character
+router.patch('/:id/edit', characterEditPATCH)
+// Eliminar a character
+router.delete('/:id/delete', characterDELETE)
+
 
 module.exports = router;
